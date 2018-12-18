fbc02b839818554075559e2a4e50c904bf561c1e
AL FATAH KADER <alfatah.kader@gmail.com>
2018-12-18 02:05:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,18 @@
 def roll_call_dwarves(dwarves)
+  
         dwarves.each_with_index { |dwarf, index|
-    puts (index + 1).to_s + ". " + dwarf }
+        
+        puts (index + 1).to_s + ". " + dwarf }
 end
 
 def summon_captain_planet(planeteer_calls)
+  
     planeteer_calls.map { |pc| pc.capitalize + "!" }
 end
 
 def long_planeteer_calls(planeteer_calls)
+  
     planeteer_calls.any? {|word| word.length > 4}
 end
+
+
